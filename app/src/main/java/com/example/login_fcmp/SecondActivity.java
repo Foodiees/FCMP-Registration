2d14362fd8ed6e48078742f14db7d07159d6853a
Syed_Affan_Hameed <syedaffanhameed@gmail.com>
2020-04-27 22:40:34 -0700

    Adds map feature

--- app/src/main/java/com/example/login_fcmp/SecondActivity.java
+++ app/src/main/java/com/example/login_fcmp/SecondActivity.java
@@ -1,14 +1,36 @@
 package com.example.login_fcmp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class SecondActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SecondActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng Hassan = new LatLng(13.008582, 76.096635);
+        mMap.addMarker(new MarkerOptions().position(Hassan).title("Marker in Hassan in India"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Hassan));
     }
 }
