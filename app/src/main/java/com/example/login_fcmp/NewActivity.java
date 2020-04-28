22b44a85b6b9ac6c35e56eb13ebe254a2fe4e118
Syed_Affan_Hameed <syedaffanhameed@gmail.com>
2020-04-28 07:41:12 -0700

    Perfect

--- app/src/main/java/com/example/login_fcmp/NewActivity.java
+++ app/src/main/java/com/example/login_fcmp/NewActivity.java
@@ -7,23 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Role_assign extends AppCompatActivity {
-    private Button order;
-
-
+public class NewActivity extends AppCompatActivity {
+    private Button cart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_role_assign);
-        order=(Button)findViewById(R.id.button3);
-
-        order.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_new);
+        cart=(Button)findViewById(R.id.button5);
+        cart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Role_assign.this,NewActivity.class));
+                startActivity(new Intent(NewActivity.this,ItemsActivity.class));
 
             }
         });
-
     }
 }
